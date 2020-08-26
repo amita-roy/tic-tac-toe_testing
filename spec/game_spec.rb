d142120804f4f7c39914195c99d481c17f8d8bfb
Amita <sardana.amita2@gmail.com>
2020-08-26 19:47:57 +0530

    add tests for winner? method in game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,4 +13,29 @@ describe Game do
       game.winner?([1, 4, 7])
     end
   end
+
+  context '#winner?' do
+    def winning_combination
+      [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ]
+    end
+
+    it 'returns true or false' do
+      expect(game.winner?([1, 4, 7])).to be true
+      expect(game.winner?([1, 2, 5])).to be false
+    end
+
+    it 'checks if winning_combination includes array_of_indexes' do
+      expect(winning_combination).to include([1, 4, 7])
+      expect(winning_combination.include?([2, 3, 5])).to be false
+    end
+  end
 end
